250f9567b9ae4953513fed9a623788baa78b7fc8
NYCDOE <admin@AC1MM60H1F68C.local>
2016-04-02 12:28:50 -0400

    Uploaded Week3Scripts

---
+++ Week3Scripts/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+	public int Score;
+	// Update is called once per frame
+	void Update () {
+		float speed = 15 * Time.deltaTime; //Our Speed in seconds
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			Debug.Log ("Up Arrow was Pressed");
+			transform.Translate (Vector3.forward * speed);
+		}	
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			Debug.Log ("Down Arrow was Pressed");
+			transform.Translate (Vector3.back * speed);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			Debug.Log ("Left Arrow Pressed");
+			transform.Translate (Vector3.left * speed);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			Debug.Log ("Right Arrow Pressed");
+			transform.Translate (Vector3.right * speed);
+		}
+	
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "coin") {
+			Score++;
+			Debug.Log (Score);
+			Destroy (other.gameObject);
+		}
+	}
+			
+}
---
+++ Week3Scripts/startifstatements.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startifstatements : MonoBehaviour {
+
+	public bool fact = true;
+	// Use this for initialization
+	void Start () {
+		if (fact == true) {
+			fact = false;
+			if (fact == false) {
+				Debug.Log ("Fact was switched to false");
+			} else {
+				Debug.Log ("ERROR");
+			}
+		} else {
+			Debug.Log ("Condition wasn't met");
+		}
+
+
+		//multi If Statement
+//		if (10 < 20) {
+//			Debug.Log (3+4*5+1);
+//		}
+//
+//		if (fact == true) {
+//			Debug.Log (fact);
+//		}
+//		if (5 < 20) {
+//			fact = false;
+//			Debug.Log (fact);
+//		}
+
+		//Standard IfElse Statement
+		//		if (10 < 20) {
+		//			Debug.Log (3+4*5+1);
+		//		}
+
+//		 else {
+//			Debug.Log (1*8+(10-5));
+//		
+//	}
+	}
+	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+}
